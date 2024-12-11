0f8ed5f6d8ccdc35ad660c0aac84c1f0e68f5548
superpuper32 <pochitalin_m@mail.ru>
2024-12-11 16:23:41 +0300

    STEP-2: fetch img  from loader added

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,12 +13,15 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
 
 const srcFilename = 'before.html';
+const imgFilename = 'nodejs.png';
 
 let data;
+let img;
 let tempDir;
 
 beforeAll(async () => {
   data = await fs.readFile(getFixturePath(srcFilename), 'utf8');
+  img = await fs.readFile(getFixturePath(imgFilename), 'utf8');
 });
 
 beforeEach(async () => {
@@ -28,7 +31,9 @@ beforeEach(async () => {
 it('pageLoader main logic', async () => {
   const scope = nock(/ru\.hexlet\.io/)
     .get('/courses')
-    .reply(200, data);
+    .reply(200, data)
+    .get('/assets/professions/nodejs.png')
+    .reply(200, img);
 
   const pagePath = await pageLoader('https://ru.hexlet.io/courses', tempDir);
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import fsp from 'fs/promises';
+import path from 'path';
 import axios from 'axios';
+import * as cheerio from 'cheerio';
 
 import { getFilenameFromUrl, getPath } from './utils.js';
 
@@ -9,11 +11,41 @@ const pageLoader = (url, dir) => {
   const filename = getFilenameFromUrl(url, '.html');
   const filepath = getPath(dir, filename);
 
+  const assetsName = getFilenameFromUrl(url, '_files');
+  const assetsPath = getPath(dir, assetsName);
+
+  // > make resources dir
+  // >> const imgUrl = ''; // cheerio find
+  // -> get src [img] from html
+  // --> get img
+  // ---> buildName [name] from src
+  // ----> save img
+  // --->
+  // ------> select tags
+  // -------> rename file
+  // ----->
+
   return axios.get(url)
     .then((res) => fsp.mkdir(projectFolder, { recursive: true })
       .then(() => res))
     .then((res) => fsp.writeFile(filepath, res.data)
-      .then(() => filepath))
+      .then(() => res))
+    .then((response) => fsp.mkdir(assetsPath, { recursive: true })
+      .then(() => response))
+    .then((res) => {
+      const $ = cheerio.load(res.data);
+      const $img = $('img');
+      const imgs = $img.toArray().map((link) => link.attribs.src);
+
+      const imgUrl = new URL(imgs[0], url);
+
+      return { imgUrl, imgName: getFilenameFromUrl(imgUrl.href, '') };
+    })
+    .then(({ imgUrl, imgName }) => {
+      axios.get(imgUrl, { responseType: 'arraybuffer' })
+        .then(({ data }) => fsp.writeFile(path.join(assetsPath, imgName), data));
+    })
+    .then(() => filepath)
     .catch(console.log)
     .finally(() => console.log(`open ${filepath}`));
 };
